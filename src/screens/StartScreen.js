385d66b6461385daf9f0668b9d170d1ddc36ce21
EduardoSQ <eduardoooo89@hotmail.com>
2023-11-06 20:07:05 -0600

    Crear pantalla de inicio

---
+++ src/screens/StartScreen.js
@@ -0,0 +1,43 @@
+import { View, Text, StyleSheet, Image, TouchableOpacity } from 'react-native'
+import React from 'react'
+
+import EscudoCuceiSrc from './../../images/EscudoCucei.png'
+
+export default function StartScreen() {
+  return (
+    <View style={styles.pantalla}>
+      <Text  style={{fontSize: 45, textAlign: "center"}}>Sistema de votación</Text>
+      <Image
+        style={styles.EscudoCucei}
+        source={EscudoCuceiSrc}
+      />
+      <TouchableOpacity style={styles.BotonIniciar}>
+        <View>
+          <Text style={{fontSize: 45}}>Iniciar</Text>
+        </View>
+      </TouchableOpacity>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+  BotonIniciar: {
+    backgroundColor: "orange",
+    alignSelf: "center",
+    borderRadius: 20,
+    //top: 25
+    top: "3.5%"
+  },
+
+  EscudoCucei: {
+    alignSelf: "center",
+    height: "70%",
+    width: "87%",
+  },
+
+  pantalla: {
+    backgroundColor: "blue",
+    height: "100%"
+  }
+
+});
