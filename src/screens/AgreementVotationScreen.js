d87130c881cd2d0f544dea4ac40c0a30bf1aba7d
EduardoSQ <eduardoooo89@hotmail.com>
2023-11-10 00:26:13 -0600

    Recibir json con acuerdos del servidor remoto

---
+++ src/screens/AgreementVotationScreen.js
@@ -0,0 +1,33 @@
+import { View, Text, Alert, CheckBox } from 'react-native'
+import React, { useEffect, useState } from 'react'
+
+const LOAD_AGREEMENTS_ERROR = "Error al cargar los acuerdos"
+
+export default function AgreementVotationScreen() {
+
+	let jsonAgreements = [];
+
+	fetch("https://lalosuperwebsite.000webhostapp.com/Proyecto%20Progra%20Internet/Acuerdos.json")
+	.then(response => {
+		if(response.ok)
+			return response.json();
+		else
+		throw new Error(LOAD_AGREEMENTS_ERROR);
+	})
+	.then(json_agreements => {
+		jsonAgreements = json_agreements;
+		console.log(jsonAgreements);
+	})
+	.catch(error => {
+		console.error(error);
+		Alert.alert(error);
+	});
+
+
+
+    return (
+        <View>
+            <Text>AgreementVotationScreen</Text>
+        </View>
+    )
+}
