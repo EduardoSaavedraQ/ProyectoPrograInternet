f702ce196cf0de0ebddadea72983a1c26970560c
EduardoSQ <eduardoooo89@hotmail.com>
2023-11-10 23:19:27 -0600

    Mostrar acuerdo en pantalla

--- src/screens/AgreementVotationScreen.js
+++ src/screens/AgreementVotationScreen.js
@@ -4,31 +4,50 @@ import React, { useEffect, useState } from 'react'
 const LOAD_AGREEMENTS_ERROR = "Error al cargar los acuerdos"
 
 export default function AgreementVotationScreen() {
-
-	let jsonAgreements = [];
-
-	fetch("https://lalosuperwebsite.000webhostapp.com/Proyecto%20Progra%20Internet/Acuerdos.json")
-	.then(response => {
-		if(response.ok)
-			return response.json();
-		else
-		throw new Error(LOAD_AGREEMENTS_ERROR);
-	})
-	.then(json_agreements => {
-		jsonAgreements = json_agreements;
-		console.log(jsonAgreements);
-		console.log(Object.keys(jsonAgreements).length);
-	})
-	.catch(error => {
-		console.error(error);
-		Alert.alert(error);
+	const [agreementsVariables, setAgreementsVariables] = useState({
+		agreements: {},
+		indexAgreements: 0,
+		totalNumberAgreements: 0,
+		vote: 0
 	});
 
+	const updateAgreements = (newJson) => {
+		setAgreementsVariables(previousState => {
+			return { ...previousState, agreements: newJson, totalNumberAgreements: Object.keys(newJson).length }
+		});
+	}
 
+	const updateAndLogAgreements = (json) => {
+        updateAgreements(json);
+        console.log("Acuerdos actualizados:", json);
+        console.log("Número total de acuerdos:", Object.keys(json).length);
+    }
+
+	useEffect(() => {
+		fetch("https://lalosuperwebsite.000webhostapp.com/Proyecto%20Progra%20Internet/Acuerdos.json")
+		.then(response => {
+			if(response.ok) 
+				return response.json();
+			else
+				throw new Error(LOAD_AGREEMENTS_ERROR);
+		})
+		.then(json => {
+			updateAndLogAgreements(json);
+		})
+		.catch(error => {
+			console.error(error);
+			Alert.alert(error);
+		})
+	}, []);
 
     return (
         <View>
-            <Text>AgreementVotationScreen</Text>
+            { Object.keys(agreementsVariables.agreements).length > 0? (
+				<Text>Acuerdo: {agreementsVariables.agreements[agreementsVariables.indexAgreements].Acuerdo}</Text>
+			) : (
+				<Text>Cargando acuerdo...</Text>
+			) }
+
         </View>
     )
 }
