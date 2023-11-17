70d57a2be612f191b5aa29f40319bf76b1d4bbf5
EduardoSQ <eduardoooo89@hotmail.com>
2023-11-17 00:25:05 -0600

    Consultar BD para obtener acuerdo actual por el cual votar

--- src/screens/AgreementVotationScreen.js
+++ src/screens/AgreementVotationScreen.js
@@ -2,32 +2,48 @@ import { View, Text, Alert, Image, StyleSheet } from 'react-native'
 import React, { useEffect, useState } from 'react'
 import VoteSection from '../components/VoteSection';
 
-const LOAD_AGREEMENTS_ERROR = "Error al cargar los acuerdos";
+//const LOAD_AGREEMENTS_ERROR = "Error al cargar los acuerdos";
+const LOAD_AGREEMENT_ERROR = "Error al cargar el acuerdo";
 
 //import bgSRC from "./../../images/metalgray_background.jpg";
 import bgSRC from "./../../images/gray_background.jpeg";
 
 export default function AgreementVotationScreen() {
-	const [agreementsVariables, setAgreementsVariables] = useState({
-		agreements: {},
-		indexAgreements: 0,
-		totalNumberAgreements: 0,
-		vote: 0
-	});
+	const [currentAgreement, setCurrentAgreement] = useState("");
 
-	const updateAgreements = (newJson) => {
+	/*const updateAgreement = (newJson) => {
 		setAgreementsVariables(previousState => {
 			return { ...previousState, agreements: newJson, totalNumberAgreements: Object.keys(newJson).length }
 		});
 	}
 
-	const updateAndLogAgreements = (json) => {
+	const updateAndLogAgreement = (json) => {
         updateAgreements(json);
         console.log("Acuerdos actualizados:", json);
-        console.log("Número total de acuerdos:", Object.keys(json).length);
-    }
+        //console.log("Número total de acuerdos:", Object.keys(json).length);
+    }*/
 
-	useEffect(() => {
+	const updateAndLogCurrentAgreement = (json) => {
+		setCurrentAgreement(json['current_agreement']);
+		console.log(currentAgreement);
+		return 1;
+	}
+
+	setInterval(() => {
+		fetch("https://lalosuperwebsite.000webhostapp.com/Proyecto%20Progra%20Internet/obtener_acuerdo_actual.php")
+		.then(response => {
+			if(response.ok)
+				return response.json()
+			else
+				throw new Error(LOAD_AGREEMENT_ERROR)
+		})
+		.then(json => {
+			updateAndLogCurrentAgreement(json);
+		})
+		.catch(error => console.error(error))
+	}, 5000);
+
+	/* useEffect(() => {
 		fetch("https://lalosuperwebsite.000webhostapp.com/Proyecto%20Progra%20Internet/Acuerdos.json")
 		.then(response => {
 			if(response.ok) 
@@ -43,13 +59,13 @@ export default function AgreementVotationScreen() {
 			console.error(error);
 			Alert.alert(error);
 		})
-	}, []);
+	}); */
 
     return (
         <View>
 			<Image source={bgSRC} style={styles.background}/>
-            { Object.keys(agreementsVariables.agreements).length > 0? (
-				<Text style={styles.agreement}>Acuerdo: {agreementsVariables.agreements[agreementsVariables.indexAgreements].Acuerdo}</Text>
+            { currentAgreement !== ""? (
+				<Text style={styles.agreement}>Acuerdo: {currentAgreement}</Text>
 				
 			) : (
 				<Text style={styles.agreement}>Cargando acuerdo...</Text>
