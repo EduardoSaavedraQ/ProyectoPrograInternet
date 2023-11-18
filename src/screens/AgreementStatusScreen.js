9d8749402b152a5c565a402bf5dee2c20bd747ac
EduardoSQ <eduardoooo89@hotmail.com>
2023-11-18 15:33:36 -0600

    Mostrar estatus de acuerdo en pantalla y actualizarlo cada cinco segundos

--- src/screens/AgreementStatusScreen.js
+++ src/screens/AgreementStatusScreen.js
@@ -1,38 +1,69 @@
-import { View, Text } from 'react-native'
-import React, { useState, useEffect } from 'react'
+import { View, Text, Alert } from 'react-native';
+import React, { useState, useEffect, useRef } from 'react';
 
-const LOAD_AGREEMENT_ERROR = "Error al cargar el acuerdo"
+const LOAD_AGREEMENT_ERROR = "Error al cargar el acuerdo";
+const LOAD_STATUS_AGREEMENT_ERROR = "Error al cargar el estatus del acuerdo";
 
 export default function AgreementStatusScreen({ navigation }) {
-    const [currentAgreement, setCurrentAgreement] = useState("")
+  const [currentAgreement, setCurrentAgreement] = useState("");
+  const [currentAgreementStatus, setCurrentAgreementStatus] = useState({
+    inFavor: '0',
+    against: '0',
+    abstention: '0'
+  });
 
-    useEffect(() => {
-        fetch("https://lalosuperwebsite.000webhostapp.com/Proyecto%20Progra%20Internet/obtener_acuerdo_actual.php")
-        .then(response => {
-            if(response.ok)
-                return response.json();
-            throw new Error(LOAD_AGREEMENT_ERROR);
-        })
-        .then(json => setCurrentAgreement(json['current_agreement']))
-        .catch(error => {
-            console.error(error);
-            if(error === LOAD_AGREEMENT_ERROR)
-                Alert.alert(error);
+  useEffect(() => {
+    fetch("https://lalosuperwebsite.000webhostapp.com/Proyecto%20Progra%20Internet/obtener_acuerdo_actual.php")
+      .then(response => {
+        if (response.ok) return response.json();
+        throw new Error(LOAD_AGREEMENT_ERROR);
+      })
+      .then(json => setCurrentAgreement(json['current_agreement']))
+      .catch(error => {
+        console.error(error);
+        if (error === LOAD_AGREEMENT_ERROR) Alert.alert(error);
+      });
+  }, []);
+
+  /* useEffect(() => {
+    const intervalId = setInterval(async () => {
+      try {
+        const response = await fetch("https://lalosuperwebsite.000webhostapp.com/Proyecto%20Progra%20Internet/obtener_estatus_acuerdo_actual.php");
+
+        if (!response.ok) throw new Error(LOAD_STATUS_AGREEMENT_ERROR);
+
+        const json = await response.json();
+
+        setCurrentAgreementStatus({
+          inFavor: json['a_favor'],
+          against: json['en_contra'],
+          abstention: json['abstencion']
         });
-    }, []);
 
-    
+        console.log(currentAgreementStatus);
+
+      } catch (error) {
+        console.log(error);
+        Alert.alert(error);
+      }
+
+    }, 5000);
+
+    // Limpiar el intervalo cuando el componente se desmonta
+    return () => clearInterval(intervalId);
+  }, []); // Asegúrate de pasar un array de dependencias vacío para que se ejecute solo una vez */
 
   return (
     <View>
-        {
-        (currentAgreement === "")? 
-            <Text>Cargando acuerdo</Text>
+      {(currentAgreement === "") ?
+        <Text>Cargando acuerdo</Text>
         :
-            <Text>Acuerdo actual: {currentAgreement}</Text>
-        }
+        <Text>Acuerdo actual: {currentAgreement}</Text>
+      }
 
-        
+      <Text>A favor: {currentAgreementStatus.inFavor}</Text>
+      <Text>En contra: {currentAgreementStatus.against}</Text>
+      <Text>Abstenerce: {currentAgreementStatus.abstention}</Text>
     </View>
-  )
+  );
 }
